992eef0867f31e7a26ab39f053b72f937d0c460a
dhiren.vajani <dhiren.vajani@stl.tech>
2021-04-18 17:35:06 +0530

    Change in JAVA File commit 1

--- src/main/java/com/devopsdemo/utilities/PropertyHelper.java
+++ src/main/java/com/devopsdemo/utilities/PropertyHelper.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Properties;
 @SuppressWarnings("unchecked")
 
+
 /**
  * Helper Class to load Properties from a property file to be passed to caller for execution. 
  * Multiple properties can be loaded. 
